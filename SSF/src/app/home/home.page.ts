f8c8dcb1c91a71c31608dbaafc2a5179220673c0
Joe-Wehbe <joewehbe@yahoo.com>
2023-11-09 01:52:10 +0200

    Finished designing the login page

--- SSF/src/app/home/home.page.ts
+++ SSF/src/app/home/home.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  selectedOption: string = 'SSF'; // Set the default option here
+
+  password: string = '';
+  showPassword: boolean = false;
+  iconToShow: string = 'eye'; // Initial icon name
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+    this.iconToShow = this.showPassword ? 'eye-off' : 'another-icon-name'; // Replace 'another-icon-name' with your desired icon
+  }
+
   constructor() {}
 
 }
