04d476c2dc7f39c7fc5314ac59d8f533649c4d73
Joe-Wehbe <joewehbe@yahoo.com>
2024-01-25 18:48:49 +0200

    Tabs <> Finished desgning the standby page

--- src/app/pages/standby/standby.page.ts
+++ src/app/pages/standby/standby.page.ts
@@ -9,6 +9,36 @@ export class StandbyPage implements OnInit {
   
   selectedSegment: string = 'Ongoing';
 
+    public actionSheetButtons = [
+    {
+      text: 'Case Report',
+      data: {
+        action: 'report',
+      },
+    },
+    {
+      text: 'Vitals History',
+      data: {
+        action: 'vitals',
+      },
+    },
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
