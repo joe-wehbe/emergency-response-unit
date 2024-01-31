11d8e099e2d667ae6f015285606b0def87a948ac
Joe-Wehbe <joewehbe@yahoo.com>
2024-01-31 02:58:45 +0200

    Tabs <> Worked on the design of emergency-details

--- src/app/pages/emergency-details/emergency-details.module.ts
+++ src/app/pages/emergency-details/emergency-details.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { EmergencyDetailsPageRoutingModule } from './emergency-details-routing.module';
 
 import { EmergencyDetailsPage } from './emergency-details.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     EmergencyDetailsPageRoutingModule
   ],
   declarations: [EmergencyDetailsPage]
