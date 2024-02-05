b538f046fcd9179e5fb2c553397d117a2e888898
Joe-Wehbe <joewehbe@yahoo.com>
2024-02-05 18:01:25 +0200

    Tabs <> Added vitals highlight in emergency-details and worked on the on-scene page

--- src/app/pages/emergency-details/emergency-details.page.ts
+++ src/app/pages/emergency-details/emergency-details.page.ts
@@ -9,6 +9,15 @@ import { Router } from '@angular/router';
 })
 export class EmergencyDetailsPage implements OnInit {
 
+  HeartRate: number = 120;
+  RespirationRate: number = 27;
+  BloodSaturation: number = 92;
+  CapillaryRefill: number = 4;
+  Temperature: number = 38.6;
+  HemoGlucoTest: number = 40;
+  Systolic: number = 160;
+  Diastolic: number = 70;
+
   constructor(private router:Router) { }
 
   ngOnInit() {
