b4c0c47b67305b43cbf4104dd4372c4f962de6d5
Theresa <theresaroukaibe1@outlook.com>
2024-02-27 17:41:40 +0200

    Backend <> Fixed Files

--- Backend/app/Http/Controllers/AdminController.php
+++ Backend/app/Http/Controllers/AdminController.php
@@ -3,8 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Announcement;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    //
+    function deleteAnnouncement(Request $request) {
+
+        //validate the params 
+       $request->validate([
+           'announcement_id' => 'required|integer',
+           'admin_id' => 'required|integer'
+        ]);
+
+        $announcement = Announcement::where('id', $request->input('announcement_id'))->first();
+
+        $admin = User::where('id', $request->input('admin_id'))->first();
+
+//check if announcement exists
+if (!$announcement) {
+    return response()->json([
+        'status' => 'Error',
+        'message' => 'Announcement not found'
+    ]);
+}
+
+        //check if admin exists 
+        if (!$admin) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Admin not found'
+            ]);
+        }
+
+        //check if user who initiated request is an admin 
+        if ($admin->user_type != 3) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'User is not an admin'
+            ]);
+        }
+
+        //check if the user who initiated the request is the one who posted the announcement
+        if ($admin->id != $announcement->admin_id) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'User does not match admin ID of poster'
+            ]);
+        }
+
+        
+
+
+        $announcement->delete();
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Announcement deleted successfully'
+        ]);
+
+    }
+
+    function deleteUser(Request $request) {
+
+        try {
+            $user = User::where('id', $request->input('user_id'))->first();
+            $admin = User::where('id', $request->input('admin_id'))->first();
+
+            //check if admin exists 
+        if (!$admin) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Admin not found'
+            ]);
+        }
+
+        //check if user who initiated request is an admin 
+        if ($admin->user_type != 3) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'User is not an admin'
+            ]);
+        }
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 'Error',
+                    'message' => 'User not found'
+                ]);
+            }
+
+            $user->delete();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User deleted successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'An error occurred while deleting the user',
+                'error' => $e->getMessage()
+            ]);
+        }
+
+    }
 }
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::group(["prefix" => "v0.1"], function(){
+        Route::group(["prefix" => "user"], function(){
+           //write the route of the apis in the user controller here 
+        });
+    
+        Route::group(["prefix" => "emergency"], function(){
+           
+           //write the route of the apis in the emergency controller here 
+
+        });
+    
+        Route::group(["prefix" => "admin"], function(){
+             //write the route of the apis in the admin controller here 
+             Route::post("delete_announcement", [AdminController::class, "deleteAnnouncement"]);
+             Route::post("delete_user", [AdminController::class, "deleteUser"]);
+        });
+
+});
+
