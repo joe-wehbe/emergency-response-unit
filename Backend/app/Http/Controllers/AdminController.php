b73c74561672da14ec8bf3c5a02d66056e038cb2
Theresa <theresaroukaibe1@outlook.com>
2024-02-28 16:06:38 +0200

    Backend <> Finished add medical FAQ API

--- Backend/app/Http/Controllers/AdminController.php
+++ Backend/app/Http/Controllers/AdminController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Announcement;
 use App\Models\User;
+use App\Models\Medical_faq;
 
 class AdminController extends Controller
 {
@@ -106,5 +108,63 @@ if (!$announcement) {
             ]);
         }
 
+    }
+
+    function addFaq(Request $request){
+        $admin = User::where('id', $request->input('admin_id'))->first();
+
+        //checking admin authorities 
+        if (!$admin) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Admin not found'
+            ]);
+        }
+
+        if ($admin->user_type != 3) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'User is not an admin'
+            ]);
+        }
+
+          $validator = Validator::make($request->all(), [
+            'type' => 'required|string',
+            'question' => 'required|string',
+            'answer' => 'required|string',
+            
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $validator->errors()->first()
+            ]);
+        }
+
+        // Check if the FAQ type is valid 
+        $validTypes = ['Patient Assessment', 'Vital Signs', 'Cardio', 'Respiration', 'Neuro', 'Blsd', 'Trauma', 'Bleeding']; 
+        if (!in_array($request->type, $validTypes)) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'No such FAQ section exists'
+            ]);
+        }
+    
+        $faq = Medical_faq::create([
+            'type' => $request->input('type'),
+            'question' => $request->input('question'),
+            'answer' => $request->input('answer'),
+        ]);
+    
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'FAQ added successfully'
+        ]);
+
+    }
+
+    function deleteFaq(Request $request){
+        
     }
 }
