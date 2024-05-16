6d1da1cec5b6feaafb7ccc1a0a91ef2e932f863d
Joe-Wehbe <joewehbe@yahoo.com>
2024-05-17 02:56:20 +0300

    Final <> Made code improvements

--- Frontend/src/app/services/firebase/fcm.service.ts
+++ Frontend/src/app/services/firebase/fcm.service.ts
@@ -20,110 +20,58 @@ export class FcmService {
 
   /****************** PUSH NOTIFICATIONS IMPLEMENTATION ******************/
 
-  private _redirect = new BehaviorSubject<any>(null);
-
-  get redirect() {
-    return this._redirect.asObservable();
-  }
-
   initializePushNotifications() {
     if(Capacitor.getPlatform() !== 'web') {
-      this.registerPush();
-      // this.getDeliveredNotifications();
+      this.registerToPushNotifications();
     }
   }
 
-  private async registerPush() {
+  private async registerToPushNotifications() {
     try {
       await this.addListeners();
-      let permStatus = await PushNotifications.checkPermissions();
+      let permissionStatus  = await PushNotifications.checkPermissions();
   
-      if (permStatus.receive === 'prompt') {
-        permStatus = await PushNotifications.requestPermissions();
+      if (permissionStatus .receive === 'prompt') {
+        permissionStatus  = await PushNotifications.requestPermissions();
       }
   
-      if (permStatus.receive !== 'granted') {
+      if (permissionStatus .receive !== 'granted') {
         throw new Error('User denied permissions!');
       }
-  
       await PushNotifications.register();
     } catch(e) {
       console.log(e);
     }
   }
-  
-  async getDeliveredNotifications() {
-    const notificationList = await PushNotifications.getDeliveredNotifications();
-    console.log('delivered notifications', notificationList);
-  }
-  
+    
   addListeners() {
-    PushNotifications.addListener(
-      'registration',
-      async(token: Token) => {
+    PushNotifications.addListener('registration', async (token: Token) => {
         console.log('My token: ', token);
         const fcm_token = token?.value;
-        let go = 1;
-        const saved_token = JSON.parse(localStorage.getItem('FCM_TOKEN') || 'null');
-        if(saved_token) {
-          if(fcm_token == saved_token) {
-            console.log('same token');
-            go = 0;
-          } else {
-            go = 2;
-          }
-        }
-        if(go == 1) {
-          // save token
-          localStorage.setItem('FCM_TOKEN', JSON.stringify(fcm_token));
-          this.saveFcmToken(JSON.stringify(fcm_token));
-        } else if(go == 2) {
-          // update token
-          const data = {
-            expired_token: saved_token,
-            refreshed_token: fcm_token
-          };
-          localStorage.setItem('FCM_TOKEN', JSON.stringify(fcm_token));
-          this.saveFcmToken(JSON.stringify(fcm_token));
+        const saved_token = JSON.parse(localStorage.getItem('fcm_token') || 'null');
+        
+        if (!saved_token || fcm_token !== saved_token) {
+          localStorage.setItem('fcm_token', JSON.stringify(fcm_token));
+          this.saveFcmToken(fcm_token);
         }
       }
-    );
+    );    
   
     PushNotifications.addListener('registrationError', (error: any) => {
-      console.log('Error: ' + JSON.stringify(error));
+      console.log('Registration Error: ' + JSON.stringify(error));
     });
   
-    PushNotifications.addListener(
-      'pushNotificationReceived',
-      async (notification: PushNotificationSchema) => {
+    PushNotifications.addListener('pushNotificationReceived', async (notification: PushNotificationSchema) => {
         console.log('Push received: ' + JSON.stringify(notification));
-        const data = notification?.data;
-        if(data?.redirect) this._redirect.next(data?.redirect);
       }
     );
   
-    PushNotifications.addListener(
-      'pushNotificationActionPerformed',
-      async (notification: ActionPerformed) => {
-        const data = notification.notification.data;
-        console.log('Action performed: ' + JSON.stringify(notification.notification));
-        console.log('push data: ', data);
-        if(data?.redirect) this._redirect.next(data?.redirect);
+    PushNotifications.addListener('pushNotificationActionPerformed', async (notification: ActionPerformed) => {
+        console.log('Action performed: ' + JSON.stringify(notification));
       }
     );
   }
   
-  async removeFcmToken() {
-    try {
-      const saved_token = JSON.parse(localStorage.getItem('FCM_TOKEN') || 'null');
-      localStorage.removeItem('FCM_TOKEN');
-    } catch(e) {
-      console.log(e);
-      throw(e);
-    }
-  }
-  
-
   /****************** APIs IMPLEMENTATION ******************/
 
   private saveFcmToken(fcmToken: string) {
