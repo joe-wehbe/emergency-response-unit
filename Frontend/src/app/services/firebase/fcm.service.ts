887c1d8f10cb874d23fc77579c4191bf64f15b2d
Joe-Wehbe <joewehbe@yahoo.com>
2024-05-17 01:37:45 +0300

    Final <> Fixed push notifications

--- Frontend/src/app/services/firebase/fcm.service.ts
+++ Frontend/src/app/services/firebase/fcm.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { ActionPerformed, PushNotifications, PushNotificationSchema, Token } from '@capacitor/push-notifications';
-import { forkJoin } from 'rxjs';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 
 export const FCM_TOKEN = 'push_notification_token';
 
@@ -20,46 +20,109 @@ export class FcmService {
 
   /****************** PUSH NOTIFICATIONS IMPLEMENTATION ******************/
 
+  private _redirect = new BehaviorSubject<any>(null);
+
+  get redirect() {
+    return this._redirect.asObservable();
+  }
+
   initializePushNotifications() {
-    if (Capacitor.getPlatform() !== 'web') {
-      this.registerToPushNotifications();
+    if(Capacitor.getPlatform() !== 'web') {
+      this.registerPush();
+      // this.getDeliveredNotifications();
     }
   }
 
-  private async registerToPushNotifications() {
-    let permissionStatus = await PushNotifications.checkPermissions();
-
-    if (permissionStatus.receive === 'prompt') {
-      permissionStatus = await PushNotifications.requestPermissions();
-    }
-    if (permissionStatus.receive !== 'granted') {
-      throw new Error('User denied permissions!');
-    }
-    await PushNotifications.register();
-  }
-
-  addListeners() { 
-    PushNotifications.addListener('registration', async (token: Token) => {
-      const incomingToken = token?.value;
-      const existingToken = localStorage.getItem(FCM_TOKEN);
-      if (incomingToken && existingToken !== JSON.stringify(incomingToken)) {
-        localStorage.setItem(FCM_TOKEN, JSON.stringify(incomingToken));
-        this.saveFcmToken(incomingToken);
+  private async registerPush() {
+    try {
+      await this.addListeners();
+      let permStatus = await PushNotifications.checkPermissions();
+  
+      if (permStatus.receive === 'prompt') {
+        permStatus = await PushNotifications.requestPermissions();
       }
-    });
-
-    PushNotifications.addListener('registrationError', (error: any) => {
-      console.log('Registration error: ' + JSON.stringify(error));
-    });
-
-    PushNotifications.addListener('pushNotificationReceived', async (notification: PushNotificationSchema) => {
-      console.log('Push notification received: ' + JSON.stringify(notification));
-    });
-
-    PushNotifications.addListener('pushNotificationActionPerformed', async (notification: ActionPerformed) => {
-      console.log('Action performed: ' + JSON.stringify(notification));
-    });
+  
+      if (permStatus.receive !== 'granted') {
+        throw new Error('User denied permissions!');
+      }
+  
+      await PushNotifications.register();
+    } catch(e) {
+      console.log(e);
+    }
   }
+  
+  async getDeliveredNotifications() {
+    const notificationList = await PushNotifications.getDeliveredNotifications();
+    console.log('delivered notifications', notificationList);
+  }
+  
+  addListeners() {
+    PushNotifications.addListener(
+      'registration',
+      async(token: Token) => {
+        console.log('My token: ', token);
+        const fcm_token = token?.value;
+        let go = 1;
+        const saved_token = JSON.parse(localStorage.getItem('FCM_TOKEN') || 'null');
+        if(saved_token) {
+          if(fcm_token == saved_token) {
+            console.log('same token');
+            go = 0;
+          } else {
+            go = 2;
+          }
+        }
+        if(go == 1) {
+          // save token
+          localStorage.setItem('FCM_TOKEN', JSON.stringify(fcm_token));
+          this.saveFcmToken(JSON.stringify(fcm_token));
+        } else if(go == 2) {
+          // update token
+          const data = {
+            expired_token: saved_token,
+            refreshed_token: fcm_token
+          };
+          localStorage.setItem('FCM_TOKEN', JSON.stringify(fcm_token));
+          this.saveFcmToken(JSON.stringify(fcm_token));
+        }
+      }
+    );
+  
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error: ' + JSON.stringify(error));
+    });
+  
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      async (notification: PushNotificationSchema) => {
+        console.log('Push received: ' + JSON.stringify(notification));
+        const data = notification?.data;
+        if(data?.redirect) this._redirect.next(data?.redirect);
+      }
+    );
+  
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      async (notification: ActionPerformed) => {
+        const data = notification.notification.data;
+        console.log('Action performed: ' + JSON.stringify(notification.notification));
+        console.log('push data: ', data);
+        if(data?.redirect) this._redirect.next(data?.redirect);
+      }
+    );
+  }
+  
+  async removeFcmToken() {
+    try {
+      const saved_token = JSON.parse(localStorage.getItem('FCM_TOKEN') || 'null');
+      localStorage.removeItem('FCM_TOKEN');
+    } catch(e) {
+      console.log(e);
+      throw(e);
+    }
+  }
+  
 
   /****************** APIs IMPLEMENTATION ******************/
 
